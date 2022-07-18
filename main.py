940122af545d40d973acf2145fa0fe68f6fb7e5e
jojothomas1515 <jojothomas1515@gmail.com>
2022-07-18 07:23:58 +0100

    checked

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-import timeit
+import asyncio
 
-def timeer():
-    for i in range(1000000):
-        print("thank you")
 
-if __name__ == '__main__':
-    start = timeit.timeit()
-    timeer()
-    end = timeit.timeit()
-    print(end-start)
+async def function1():
+    for i in range(20):
+        if (i == 10):
+            print("now")
+            await asyncio.sleep(5)
+        await asyncio.sleep(.5)
+        print("hello from function 1")
 
+
+async def function2():
+    for j in range(10):
+        print('wording')
+        await asyncio.sleep(1)
+
+
+async def main():
+
+    loop = asyncio.get_running_loop()
+    task = loop.create_task(function1())
+    task2 = loop.create_task(function2())
+
+    await task
+    await task2
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
